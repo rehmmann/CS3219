30cc35b7352b27a86f356e155e63451dd4879401
Rehman <rehmansajid999@gmail.com>
2023-09-10 13:28:49 +0800

    Integrate dockerfiles

--- user-service/app.js
+++ user-service/app.js
@@ -1,11 +1,11 @@
 // First Test Program
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Peerprep User Service");
+app.get("/", (req, res) => {
+  res.send("Welcome to Peerprep User Service");
 });
 
-app.listen(8080, ()=> {
-    console.log("App Intialised on port 8080");
+app.listen(8080, () => {
+  console.log("App Intialised on port 8080");
 });
