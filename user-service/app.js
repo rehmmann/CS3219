0d809906f8e4eb009589961200bd3dbd9cc42ccf
Rehman <rehmansajid999@gmail.com>
2023-09-19 00:15:56 +0800

    Basic connection to Mongo

--- user-service/app.js
+++ user-service/app.js
@@ -1,6 +1,21 @@
 // First Test Program
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const uri =
+  "mongodb+srv://rehmmann:rehman@cluster0.qredme9.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log("Error connecting to MongoDB", err);
+  }
+}
+
+connect();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Peerprep User Service");
