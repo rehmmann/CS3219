309202794392b809cef40f5766015fe59fabc8f0
evanpy <evanleewj@gmail.com>
2023-09-27 02:07:34 +0800

    Add match status to redux store
    Redux store keeps track of isMatching status in order to render loading page when set to true

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -3,11 +3,13 @@ import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { api } from './api';
 import questionReducer from './slices/questionSlice';
 import userReducer from './slices/userSlice'; // temporary until hooked up with be
+import matchReducer from './slices/matchSlice';
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     questions: questionReducer,
     user: userReducer,
+    isMatching: matchReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
