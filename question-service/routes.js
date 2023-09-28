b4739a1a51de471f751d1659c75a000f78ad4a97
Rehman <rehmansajid999@gmail.com>
2023-09-29 02:38:55 +0800

    Built Question Service

---
+++ question-service/routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import Question from "./question-model.js";
+import {
+  createQuestion,
+  getRandomQuestion,
+  getFilteredQuestions,
+  deleteQuestion,
+} from "./question-controller.js";
+
+let router = express.Router();
+
+//Create new question based on request body
+router.post("/questions/new", createQuestion);
+
+//Get random question
+router.get("/questions/random", getRandomQuestion);
+
+//Get question based on query parameters
+router.get("/questions", getFilteredQuestions);
+
+//Delete question based on questionId
+router.delete("/questions/:id", deleteQuestion);
+
+export let routes = router;
