f1bb448870050aa89d35b9076b01337e144c1e25
evanpy <evanleewj@gmail.com>
2023-10-23 16:14:38 +0800

    Move editor toolbar to editor component
    Editor toolbar that consists language drop down menu and code submit button moved to from collaboration component to editor component. A prop `isMainEditor` has been added to the editor component. The new prop is used to render the toolbar as we would only change the language or submit the code of the main editor (the other editor is view only from peer's code).

--- frontend/src/components/Collaboration/Editor.tsx
+++ frontend/src/components/Collaboration/Editor.tsx
@@ -2,29 +2,71 @@ import { useState } from "react";
 
 import {Editor as MonacoEditor} from "@monaco-editor/react";
 
+import LanguageDropDown from './LanguageDropDown';
+import Button from '../../components/Button/Button';
+import Box from '@mui/material/Box';
+
+import './Editor.scss';
+
+const buttonStyle = {
+  fontSize: "0.8rem", 
+  fontWeight: '500', 
+  color:'white', 
+  border: "none", 
+  borderRadius: "5px", 
+  backgroundColor:'#83DA58', height:'100%'
+};
+
+type languageType = {
+  id: number,
+  name: string,
+  label: string,
+  value: string,
+};
+
 type EditorProps = {
-  language: string,
+  defaultLanguage: string,
+  defaultCode: string,
+  isMainEditor: boolean,
 }
 
 const Editor = (props: EditorProps) => {
-  const { language } = props;
-  const [value, setValue] = useState("");
+  const { defaultLanguage, defaultCode, isMainEditor } = props;
+  const [language, setLanguage] = useState(defaultLanguage);
+  const [code, setCode] = useState(defaultCode);
 
   const handleEditorChange = (value: string) => {
-    setValue(value);
+    setCode(value);
+  };
+
+  const handleChangeLanguage = (language: languageType) => {
+    setLanguage(language.value);
+  };
+
+  const handleSubmitCode = () => {
+    console.log(code);
   };
 
   return (
-    <MonacoEditor
-      height='40vh'
-      width={`100%`}
-      defaultLanguage={language}
-      language={language}
-      value={value}
-      theme={"light"}
-      defaultValue=""
-      onChange={handleEditorChange}
-    />
+    <>
+      {
+        isMainEditor && 
+        <Box className="editor_toolbar_container">
+          <LanguageDropDown onSelectChange={handleChangeLanguage}/>
+          <Button title={'Submit'} event={handleSubmitCode} style={buttonStyle}/>
+        </Box>
+      }
+      <MonacoEditor
+        height='40vh'
+        width={`100%`}
+        defaultLanguage={language}
+        language={language}
+        value={code}
+        theme={"light"}
+        defaultValue=""
+        onChange={handleEditorChange}
+      />
+    </>
   );
 };
 export default Editor;
